553ca8e6d84bcef0d1e9e8a573b9c909f7a10dd7
daniel garcia vaglio <degv364@gmail.com>
2016-11-13 10:42:16 -0600

    change ram to work with bytes not bits

--- ADMA/modules/ram.v
+++ ADMA/modules/ram.v
@@ -3,19 +3,19 @@
 //Pero es necesario para el testbench.
 //Lee y escribe en la direccion determinada.
 
-module ram(address,data_in, write, read, data_out, CLK);
-   input [63:0] address;
-   input [31:0] data_in;
-   input 	write;
-   input 	read;
-   input 	CLK;
+module ram(input [63:0] address,
+	   input [31:0] data_in, 
+	   input write, 
+	   input read, 
+	   output [31:0] data_out, 
+	   input CLK
+	   );
    
-   
-   
-   output [31:0] data_out;
    reg [31:0] 	 data_out;
-   
 
+   wire [63:0] 	 address_bits;
+   assign address_bits= address<<3;
+   
    reg [1024:0] info;
    wire [11:0] 	scope;
 
@@ -23,36 +23,32 @@ module ram(address,data_in, write, read, data_out, CLK);
       # 0 info=0;
    end
    
-   
    //assign address_end= address+31;
    assign scope = info[75:64];
-   
-   
    //verify mod 32, valid address. whole words
 
    always @(posedge CLK) begin
       //if (address[4:0]==0) begin
       if (address[1:0]==0)begin
 	 if (write==1) begin
-	    info[address+:32]=data_in;
+	    info[address_bits+:31]=data_in;
 	 end
 	 if (read==1) begin
-	    data_out=info[address+:32];
+	    data_out=info[address_bits+:31];
 	 end
       end
    end
 
    //Escritura de constantes en ram
    always @(CLK) begin
-      info [67:64]=2;
+      info [31:0]=1;
+
+      info [63:32]=2;
+      info [95:64]=3;
       
-      info[71:68]= 1;
-      info[75:72]=1;
       
    end
    
-   
-
 endmodule // ram
 
 
