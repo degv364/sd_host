43595d89297f9187aa3dbc10787d7fa993f242d1
josefallas0302 <jafp0302@gmail.com>
2016-11-30 00:16:05 -0600

    Registros

--- REG/BIEN/regs.v
+++ REG/BIEN/regs.v
@@ -3,7 +3,7 @@
 module reg_32(	
 output acknowledge,	// acknowledge (1- Dato Confiable, 0 - Dato no confiable )
 output [WIDTH-1 : 0] rd_data,	// Salida, de  WIDTH bits
-output busy,
+output busy,			//Alguien está escribiendo 
 
 input  [WIDTH-1 : 0] wr_data,	// Entrada, de WIDTH bits
 input clk,	// clock
@@ -16,7 +16,9 @@ parameter WIDTH = 32;
 reg busy = 0;
 reg [WIDTH-1 : 0] rd_data;
 reg acknowledge = 0;
-reg [WIDTH-1 :0] mask = 32'h0000_0000;
+//reg [WIDTH-1 :0] mask = 32'h0000_0000;
+
+wr_valid = 0;
 
 always @(*)begin
 	if(wr_valid == 1)begin
@@ -27,15 +29,13 @@ always @(*)begin
 		busy = 0;
 	end
 	
-	if(busy == 1)begin
-		acknowledge = 0;
-		mask = 32'hXXXX_XXXX;
-	end
-
-	else begin
+	if(rd_data == wr_data)begin
 		acknowledge = 1;
-		mask = 32'hFFFF_FFFF;
 	end
+	else begin
+		acknowledge = 0;
+	end
+	
 end
 
 always @(posedge clk)begin
@@ -45,28 +45,28 @@ always @(posedge clk)begin
 	end	
 
 	else begin
-		if(acknowledge == 0)begin
-			rd_data <=  32'hXXXX_XXXX;
-			end
-	
+		if(wr_valid == 1)begin
+			rd_data  <= wr_data;
+		end	
 		else begin
-			rd_data  <= mask & wr_data;
+			// Nada
 		end
+	
 	end
-end
 
 endmodule
+
+// Este módulo es una base para crear el registro del tamaño que se desee. De modo que existen reg_8, reg_16 y reg_32
 
 module reg_16(	
 output acknowledge,	// acknowledge (1- Dato Confiable, 0 - Dato no confiable )
 output [WIDTH-1 : 0] rd_data,	// Salida, de  WIDTH bits
-output busy,
+output busy,			//Alguien está escribiendo 
 
 input  [WIDTH-1 : 0] wr_data,	// Entrada, de WIDTH bits
 input clk,	// clock
 input reset,	// Devuelve a cero
 input wr_valid	// Bandera de Escritura (1 - Estoy escribiendo, 0 - Está libre )
-
 );
 
 parameter WIDTH = 16;
@@ -74,19 +74,25 @@ parameter WIDTH = 16;
 reg busy = 0;
 reg [WIDTH-1 : 0] rd_data;
 reg acknowledge = 0;
-reg [WIDTH-1 :0] mask = 16'h0000;
+
+wr_valid = 0;
 
 always @(*)begin
 	if(wr_valid == 1)begin
-		acknowledge = 0;
 		busy = 1;
-		mask = 16'hXXXX;
 	end
+
 	else begin
 		busy = 0;
-		acknowledge = 1;
-		mask = 16'hFFFF;
 	end
+	
+	if(rd_data == wr_data)begin
+		acknowledge = 1;
+	end
+	else begin
+		acknowledge = 0;
+	end
+	
 end
 
 always @(posedge clk)begin
@@ -96,46 +102,54 @@ always @(posedge clk)begin
 	end	
 
 	else begin
-		if(acknowledge == 0)begin
-			rd_data <=  16'hXXXX;
-			end
-	
+		if(wr_valid == 1)begin
+			rd_data  <= wr_data;
+		end	
 		else begin
-			rd_data  <= mask & wr_data;
+			// Nada
 		end
+	
 	end
-end
 
 endmodule
+
+// Este módulo es una base para crear el registro del tamaño que se desee. De modo que existen reg_8, reg_16 y reg_32
 
 module reg_8(	
 output acknowledge,	// acknowledge (1- Dato Confiable, 0 - Dato no confiable )
 output [WIDTH-1 : 0] rd_data,	// Salida, de  WIDTH bits
-output busy,
+output busy,			//Alguien está escribiendo 
+
 input  [WIDTH-1 : 0] wr_data,	// Entrada, de WIDTH bits
 input clk,	// clock
 input reset,	// Devuelve a cero
 input wr_valid	// Bandera de Escritura (1 - Estoy escribiendo, 0 - Está libre )
 );
 
-parameter WIDTH = 8;
+parameter WIDTH = 32;
 
 reg busy = 0;
 reg [WIDTH-1 : 0] rd_data;
 reg acknowledge = 0;
-reg [WIDTH-1 :0] mask = 16'h00;
+
+wr_valid = 0;
 
 always @(*)begin
 	if(wr_valid == 1)begin
-		acknowledge = 0;
 		busy = 1;
-		mask = 8'hXX;
 	end
+
 	else begin
 		busy = 0;
-		acknowledge = 1;
-		mask = 8'hFF;
 	end
+	
+	if(rd_data == wr_data)begin
+		acknowledge = 1;
+	end
+	else begin
+		acknowledge = 0;
+	end
+	
 end
 
 always @(posedge clk)begin
@@ -145,16 +159,14 @@ always @(posedge clk)begin
 	end	
 
 	else begin
-		if(acknowledge == 0)begin
-			rd_data <=  8'hXX;
-			end
-	
+		if(wr_valid == 1)begin
+			rd_data  <= wr_data;
+		end	
 		else begin
-			rd_data  <= mask & wr_data;
+			// Nada
 		end
+	
 	end
-end
 
 endmodule
-
 
