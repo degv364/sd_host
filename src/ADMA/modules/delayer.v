9c2f6394753e593c21852b67c63d2333b05345d3
daniel garcia vaglio <degv364@gmail.com>
2016-12-08 08:22:30 -0600

    add delayer

---
+++ src/ADMA/modules/delayer.v
@@ -0,0 +1,14 @@
+`include "defines.v"
+
+module delayer(input clk,
+	       input [31:0] data_in,
+	       output [31:0] data_out);
+   
+   reg [31:0] 		     memory;
+   
+   always @(posedge clk) begin
+
+
+      endmodule // delayer
+
+   
